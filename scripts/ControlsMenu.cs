7bb6cbbb74d9d2d53344cb9fb48c22260917db6a
Petr Mrva <mr.petr04@email.cz>
2026-04-27 09:03:44 +0200

    BUG solved

--- scripts/ControlsMenu.cs
+++ scripts/ControlsMenu.cs
@@ -3,9 +3,20 @@ using System;
 
 public partial class ControlsMenu : Control
 {
+	// Tato metoda se zavolá při jakémkoliv vstupu
+	public override void _Input(InputEvent @event)
+	{
+		// Kontrola, zda byla stisknuta klávesa Esc
+		if (@event.IsActionPressed("ui_cancel")) // "ui_cancel" je standardně namapováno na Esc
+		{
+			// Označíme vstup za zpracovaný, aby ho nezachytila jiná menu (třeba PauseMenu)
+			GetViewport().SetInputAsHandled();
+			OnBackPressed();
+		}
+	}
+
 	private void OnBackPressed()
 	{
 		GetTree().ChangeSceneToFile("res://scenes/main_menu.tscn");
-
 	}
 }
